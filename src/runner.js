d13b76b6934c1087262f3daa1540ec98b169a352
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-07-11 04:46:42 +0000

    fix: much better

--- src/runner.js
+++ src/runner.js
@@ -4,29 +4,25 @@ const runner = async api => {
   let { filename, onStart, onEnd, pipe } = api
   if (!pipe) pipe = x => x
   const pending = []
-  class TestNode {
-    constructor ({ name, fn, filename, parent }) {
-      this.name = name
-      this.fn = fn
-      this.filename = filename
-      this.parent = parent
-      pending.push(this)
-    }
-
-    add (name, fn) {
-      return pipe(new TestNode({ ...this, name, parent: this }))
-    }
+  const create = ({ name, fn, filename, parent }) => {
+    const test = (name, fn) => create({name, fn, filename, parent})
+    test.testName = name
+    test.fn = fn
+    test.filename = filename
+    test.parent = parent
+    pending.push(test)
+    return pipe(test)
   }
   if (!filename) throw new Error('No filename')
   const url = pathToFileURL(filename)
   const module = await import(url)
   if (!module.default) module.default = module.test
   if (module.default) {
-    await module.default((name, fn) => pipe(new TestNode({ name, fn, filename })))
+    await module.default((name, fn) => create({ name, fn, filename }))
   } else {
     if (module.tests) {
       for (const [name, fn] of Object.entries(module.tests)) {
-        pipe(new TestNode({ name, fn, filename }))
+        create({ name, fn, filename })
       }
     } else {
       throw new Error('This module does not export anything regonizable as a test')
